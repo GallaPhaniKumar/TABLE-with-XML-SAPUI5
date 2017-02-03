02ecd4fdd9962da40e31180c9405fbec4b9d55af
GallaPhaniKumar <gphanikumar90@gmail.com>
2017-02-03 14:08:24 +0530

    Create Formatter.js

---
+++ Formatter.js
@@ -0,0 +1,28 @@
+sap.ui.define(function() {
+	"use strict";
+
+	var Formatter = {
+		weightState : function(fValue) {
+			debugger;
+			try {
+				fValue = parseFloat(fValue);
+				if (fValue < 0) {
+					return "None";
+				} else if (fValue < 1000) {
+					return "Success";
+				} else if (fValue < 2000) {
+					return "Warning";
+				} else {
+					return "Error";
+				}
+			} catch (err) {
+				return "None";
+			}
+		}
+	};
+	return Formatter;
+},true );
+
+
+
+
